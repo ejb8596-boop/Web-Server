a6dce000ad16b27086485969d4d2bd8a3bf6d7b6
ejb8596-boop <ejb8596@nyu.edu>
2025-09-26 21:53:18 -0400

    webServer.py

--- webServer.py
+++ webServer.py
@@ -83,7 +83,3 @@ if __name__ == "__main__":
 
   #serverSocket.close()
   #sys.exit()  # Terminate the program after sending the corresponding data
-
-
-if __name__ == "__main__":
-  webServer(13331)
